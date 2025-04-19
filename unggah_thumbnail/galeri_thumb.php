2c05c73f0db6a8d0651d4f8226f5c67b555a8a27
putri06996 <putrirossaananta@gmail.com>
2025-04-19 21:20:22 +0700

    Add existing file

--- unggah_thumbnail/galeri_thumb.php
+++ unggah_thumbnail/galeri_thumb.php
@@ -87,7 +87,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <html lang="en"> 
     <head> 
         <meta charset="UTF-8"> 
-        <title>Galeri Gambar Responsive</title> 
+        <title>Upload Gambar + Thumbnail</title> 
         <link 
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
         rel="stylesheet"> 
